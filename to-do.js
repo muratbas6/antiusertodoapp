871bf73ac8dd6c85861ebe017d6c7a47ae651ae6
muratbas6 <muratbas.dev@gmail.com>
2018-08-13 19:27:39 +0300

    NOT USER FRIENDLY

---
+++ to-do.js
@@ -0,0 +1,9 @@
+function addNew() {
+  console.log($('.list-group li').length);
+  var new_to_do = prompt("What you want to add?");
+  if (new_to_do.length > 0 && new_to_do !== null) {
+    $('.list-group').append('<li class ="list-group-item list-group-item-action">' + new_to_do + '</li>');
+  }
+}
+
+
